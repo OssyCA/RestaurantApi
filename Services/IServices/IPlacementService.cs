a4e01a9f7797c4b919fc5e45ef84930b6caabf2c
Ossy <drake@OSSMAN>
2025-09-16 14:58:21 +0200

    Starting on automate table assigning

---
+++ Services/IServices/IPlacementService.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using RestaurantApi.Models;
+
+namespace RestaurantApi.Services.IServices
+{
+    public interface IPlacementService
+    {
+        Task<RestaurantTable> AssignTable(); 
+    }
+}
